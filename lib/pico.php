67b6720dc8933e10871ca32683ffdf0b9b144f15
Andrew Meyer <andrew@rewdy.com>
2013-07-26 22:24:31 -0500

    Moved the loading of config higher up in the sequence of processes so that config vars would be available before more hooks run.

--- lib/pico.php
+++ lib/pico.php
@@ -21,6 +21,10 @@ class Pico {
 		// Load plugins
 		$this->load_plugins();
 		$this->run_hooks('plugins_loaded');
+
+		// Load the settings
+		$settings = $this->get_config();
+		$this->run_hooks('config_loaded', array(&$settings));
 		
 		// Get request url and script url
 		$url = '';
@@ -51,10 +55,6 @@ class Pico {
 		}
 		$this->run_hooks('after_load_content', array(&$file, &$content));
 		
-		// Load the settings
-		$settings = $this->get_config();
-		$this->run_hooks('config_loaded', array(&$settings));
-
 		$meta = $this->read_file_meta($content);
 		$this->run_hooks('file_meta', array(&$meta));
 		$content = $this->parse_content($content);
