577160b109bb17b02483ffc333fa63b0808e72d3
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2015-06-10 11:40:26 +0200

    Apply PSR1/PSR2 coding standards.
    Mostly - use consistent indentation. Currently some methods in Pico
    class are indented with space ( get_files() ), some with tabs.

--- plugins/pico_plugin.php
+++ plugins/pico_plugin.php
@@ -7,88 +7,89 @@
  * @link http://picocms.org
  * @license http://opensource.org/licenses/MIT
  */
-class Pico_Plugin {
+class Pico_Plugin
+{
 
-	public function plugins_loaded()
-	{
-		
-	}
+    public function plugins_loaded()
+    {
 
-	public function config_loaded(&$settings)
-	{
-		
-	}
-	
-	public function request_url(&$url)
-	{
-		
-	}
-	
-	public function before_load_content(&$file)
-	{
-		
-	}
-	
-	public function after_load_content(&$file, &$content)
-	{
-		
-	}
-	
-	public function before_404_load_content(&$file)
-	{
-		
-	}
-	
-	public function after_404_load_content(&$file, &$content)
-	{
-		
-	}
-	
-	public function before_read_file_meta(&$headers)
-	{
-		
-	}
-	
-	public function file_meta(&$meta)
-	{
-		
-	}
+    }
 
-	public function before_parse_content(&$content)
-	{
-		
-	}
-	
-	public function after_parse_content(&$content)
-	{
-		
-	}
-	
-	public function get_page_data(&$data, $page_meta)
-	{
-		
-	}
-	
-	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
-	{
-		
-	}
-	
-	public function before_twig_register()
-	{
-		
-	}
-	
-	public function before_render(&$twig_vars, &$twig, &$template)
-	{
-		
-	}
-	
-	public function after_render(&$output)
-	{
-		
-	}
-	
+    public function config_loaded(&$settings)
+    {
+
+    }
+
+    public function request_url(&$url)
+    {
+
+    }
+
+    public function before_load_content(&$file)
+    {
+
+    }
+
+    public function after_load_content(&$file, &$content)
+    {
+
+    }
+
+    public function before_404_load_content(&$file)
+    {
+
+    }
+
+    public function after_404_load_content(&$file, &$content)
+    {
+
+    }
+
+    public function before_read_file_meta(&$headers)
+    {
+
+    }
+
+    public function file_meta(&$meta)
+    {
+
+    }
+
+    public function before_parse_content(&$content)
+    {
+
+    }
+
+    public function after_parse_content(&$content)
+    {
+
+    }
+
+    public function get_page_data(&$data, $page_meta)
+    {
+
+    }
+
+    public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
+    {
+
+    }
+
+    public function before_twig_register()
+    {
+
+    }
+
+    public function before_render(&$twig_vars, &$twig, &$template)
+    {
+
+    }
+
+    public function after_render(&$output)
+    {
+
+    }
+
 }
 
 ?>
