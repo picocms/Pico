df10d60846d640687fdd77a7bc6dd8d14e728fb9
Daniel Rudolf <github.com@daniel-rudolf.de>
2015-08-30 21:29:20 +0200

    Fixing paths in global.php
    That shouldn't be there... Thanks @theshka for spotting

--- global.php
+++ global.php
@@ -1,11 +1,9 @@
 <?php
-define('HTTPDOCS', realpath(rtrim(__DIR__, '/')) . '/');
-define('ROOT_DIR', realpath(HTTPDOCS . '../httpdocs-includes') . '/');
-
+define('ROOT_DIR', __DIR__ . '/');
 define('LIB_DIR', ROOT_DIR . 'lib/');
 define('VENDOR_DIR', ROOT_DIR . 'vendor/');
 define('PLUGINS_DIR', ROOT_DIR . 'plugins/');
-define('THEMES_DIR', HTTPDOCS . 'themes/');
+define('THEMES_DIR', ROOT_DIR . 'themes/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
 define('CACHE_DIR', LIB_DIR . 'cache/');
 
