5e77d862bf420abbb8df9524f38ce2e59adde2a0
Daniel Rudolf <github.com@daniel-rudolf.de>
2015-09-29 00:42:04 +0200

    Remove twig cache dir

--- global.php
+++ global.php
@@ -5,7 +5,6 @@ define('VENDOR_DIR', ROOT_DIR . 'vendor/');
 define('PLUGINS_DIR', ROOT_DIR . 'plugins/');
 define('THEMES_DIR', ROOT_DIR . 'themes/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
-define('CACHE_DIR', LIB_DIR . 'cache/');
 
 require_once(VENDOR_DIR . 'autoload.php');
 
