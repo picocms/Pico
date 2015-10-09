132399f3059e7a0e1b51f10cabad2f520ee458f3
theshka <t.heshka@gmail.com>
2015-10-09 09:39:56 -0600

    add comments/in-line docs

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+// load dependencies
 require_once(__DIR__ . '/vendor/autoload.php');
+
+// instance Pico
 $pico = new Pico(
-    __DIR__,
-    'config/',
-    'plugins/',
-    'themes/'
+    __DIR__,    // root dir
+    'config/',  // config dir
+    'plugins/', // plugins dir
+    'themes/'   // themes dir
 );
+
+// override configuration?
+// $pico->setConfig(array());
+
+// run application
 echo $pico->run();
