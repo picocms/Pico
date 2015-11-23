c3b908939f2664ad86f06b677ec50fc0757f0fbb
theshka <tyler@heshka.com>
2015-11-23 13:26:31 -0600

    update deploy-phpdoc.sh for auto docs

--- build/deploy-phpdoc.sh
+++ build/deploy-phpdoc.sh
@@ -12,11 +12,11 @@ git config user.name "Travis CI"
 git config user.email "travis-ci@picocms.org"
 
 #copy new files to release number
-cp -a $TRAVIS_BUILD_DIR/build/docs/pico-$TRAVIS_TAG $TRAVIS_BUILD_DIR/build/Pico/phpDoc/pico-$TRAVIS_TAG
-#move old files
+cp -a $TRAVIS_BUILD_DIR/build/docs/$TRAVIS_TAG $TRAVIS_BUILD_DIR/build/Pico/phpDoc/$TRAVIS_TAG
+#move old master files to old-stable
 mv -f $TRAVIS_BUILD_DIR/build/Pico/phpDoc/master $TRAVIS_BUILD_DIR/build/Pico/phpDoc/old-stable
 #copy new files to master
-cp -a $TRAVIS_BUILD_DIR/build/docs/pico-$TRAVIS_TAG $TRAVIS_BUILD_DIR/build/Pico/phpDoc/master
+cp -a $TRAVIS_BUILD_DIR/build/docs/$TRAVIS_TAG $TRAVIS_BUILD_DIR/build/Pico/phpDoc/master
 
 # Add the files to our commit
 git add $TRAVIS_BUILD_DIR/build/Pico/phpDoc/*
@@ -24,7 +24,6 @@ git add $TRAVIS_BUILD_DIR/build/Pico/phpDoc/*
 # Commit the files with our commit message
 git commit -m "Update Documentation for Pico $TRAVIS_TAG"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch.We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GITHUB_OAUTH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# Force push from the current repo's gh-pages branch to the remote repo's gh-pages branch.
+# We redirect output to /dev/null to hide any sensitive data that might otherwise be exposed.
+git push --force --quiet "https://${GITHUB_OAUTH_TOKEN}@github.com/picocms/Pico.git" master:gh-pages > /dev/null 2>&1
